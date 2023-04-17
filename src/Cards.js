063cceb0601c9f1ed465fc1801ea56642b1700bb
JumaKenn <jumaliko711@gmail.com>
2023-04-17 04:21:20 +0300

    added links to backend

--- src/Cards.js
+++ src/Cards.js
@@ -4,14 +4,14 @@ import './Cards.css';
 function Cards() {
     return (
         <div className="cards">
-            <a href="/rates" className="card-link">
+            <a href="https://django-server-production-5811.up.railway.app/rates/" className="card-link">
                 <div className="card">
                     <h2>Rates</h2>
                     <p>Get realtime construction rates</p>
                     <button>Learn More</button>
                 </div>
             </a>
-            <a href="/shop" className="card-link">
+            <a href="https://django-server-production-5811.up.railway.app/shops/" className="card-link">
                 <div className="card">
                     <h2>Shop with Us</h2>
                     <p>Buy from hardwares near you</p>
